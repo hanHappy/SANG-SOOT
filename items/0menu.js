ca50d9467fa77d87f5776245999b26e77c48cffd
hanHappy <sangmin950706@gmail.com>
2023-04-11 18:51:43 +0900

    [Menu.js] 메뉴판 버튼 구현 완료
    - 메뉴판 버튼 구현 완료~
    - 버튼 클릭 시 버튼 인덱스 = 메뉴 인덱스 되도록 조치중~

--- items/0menu.js
+++ items/0menu.js
@@ -7,7 +7,7 @@ export default class Menu {
     #ratedPrice;
     #value;
     #menuNums;
-
+    #btns;
 
     constructor() {
         this.#name = TownCanvas.rstrnts[Restaurant.rstrntIndex].menus.name;
@@ -15,6 +15,11 @@ export default class Menu {
         this.#ratedPrice = TownCanvas.rstrnts[Restaurant.rstrntIndex].menus.ratedPrice;
         this.#value = TownCanvas.rstrnts[Restaurant.rstrntIndex].menus.value;
         this.#menuNums = TownCanvas.rstrnts[Restaurant.rstrntIndex].menus.name.length;
+        this.#btns = [];
+        for(let i = 0; i < 7; i++){
+            this.#btns[i] = document.getElementById(`menuBtn${i}`);
+        }
+        console.log(this.#btns);
     }
 
     // 메뉴 출력 --------------------------------------------------------
@@ -25,7 +30,7 @@ export default class Menu {
         let value = this.#value;
         let menuNums = this.#name.length;
         ctx.font = "45px dgm";
-        ctx.fillText("메뉴            가격   평가가격   가성비", 140, 240);
+        ctx.fillText("메뉴            가격   평가가격   가성비", 170, 240);
         // 가성비 -> ★로 변환
         // 급간 : 판매 가격의 10%
         for (let i = 0; i < menuNums; i++) {
@@ -43,15 +48,19 @@ export default class Menu {
             ctx.font = "35px dgm";
             ctx.letterSpacing = "1px";
             // 메뉴명
-            ctx.fillText(name[i], 140, 65 * (i + 1) + 240);
+            ctx.fillText(name[i], 170, 65 * (i + 1) + 240);
             // 판매가격
-            ctx.fillText(p[i], 511, 65 * (i + 1) + 240);
+            ctx.fillText(p[i], 541, 65 * (i + 1) + 240);
             // 평가 가격
-            ctx.fillText(rP[i], 710, 65 * (i + 1) + 240);
+            ctx.fillText(rP[i], 740, 65 * (i + 1) + 240);
             ctx.font = "35px arial";
             ctx.letterSpacing = "-7px";
             // 가성비 (★)
-            ctx.fillText(value[i], 898, 65 * (i + 1) + 240);
+            ctx.fillText(value[i], 928, 65 * (i + 1) + 240);
+        }
+        // 버튼 활성화
+        for(let i = 0; i < menuNums; i++){
+            this.#btns[i].style.display = "block";
         }
     }
 
