d72fe6651aa6dcfe090564a848e5bd8eb737ae64
hanHappy <sangmin950706@gmail.com>
2023-04-12 17:46:35 +0900

    [restaurantCanvas.js] 식당 입장~게임 시작 전까지 완성
    - 평가 페이지 완성!
    - (미완) 유저 평가 가격 -> 누적 평가 가격에 적용해야 함

--- ui/0restaurantCanvas.js
+++ ui/0restaurantCanvas.js
@@ -30,7 +30,7 @@ export default class RestaurantCanvas {
     this.#h = 820;
 
     // Menu ---------------------------------------------------------
-    // this.#menu = new Menu();
+    this.#menu = new Menu(this.#ctx);
 
     // click ---------------------------------------------------------
     this.#sceneIndex = 0;
@@ -45,36 +45,12 @@ export default class RestaurantCanvas {
     }
   } // ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
 
-  // Scene_0
-  welcome() {
-    // 어서오세요~
-    this.#ctx.drawImage(
-      this.#scenes[this.#sceneIndex],
-      this.#x,
-      this.#y,
-      this.#w,
-      this.#h
-    );
-    let rstrntName = TownCanvas.rstrnts[Restaurant.rstrntIndex].name;
-    this.#ctx.font = "37px dgm";
-    this.#ctx.fillText(`${rstrntName}입니다!`, 330, 687);
-  }
-
   // 클릭할 때마다 다음 scene으로 넘어감
   clickHandler() {
     if (this.#sceneIndex == 1 || this.#sceneIndex == 9)
       return;
     this.#sceneIndex++;
-    let index = this.#sceneIndex;
-    let ctx = this.#ctx;
-    let scene = this.#scenes[index];
-    let x = this.#x;
-    let y = this.#y;
-    let w = this.#w;
-    let h = this.#h;
-    this.#ctx.drawImage(scene, x, y, w, h);
-
-    this.#menu = new Menu(this.#ctx);
+    this.draw();
 
     // Scene_1 : 메뉴판
     if (this.#sceneIndex == 1)
@@ -84,59 +60,69 @@ export default class RestaurantCanvas {
       this.scene9(this.#menu);
   } // click handler
 
-  scene9(menu) {
-    let name = menu.name;
-    let price = menu.price;
-    this.#ctx.font = "35px dgm";
-    // 메뉴명
-    this.#ctx.fillText(name, 122, 530);
-    // 판매가격
-    this.#ctx.fillText(price, 255, 618);
-
-    // 평가
-    let form = document.getElementById("S9-form");
-    form.style.display = "block";
-    // 입력 박스
-    let input = document.getElementById("S9-input");
-
-    // 평가 버튼
-    let submit = document.getElementById("S9-submit");
-    submit.addEventListener('click', function () {
-      menu.ratedPrice = input.value;
-      console.log(input.value);
-      
-      this.#sceneIndex++;
-      let ctx = this.#ctx;
-      let scene = this.#scenes[this.#sceneIndex];
-      let x = this.#x;
-      let y = this.#y;
-      let w = this.#w;
-      let h = this.#h;
-      this.#ctx.drawImage(scene, x, y, w, h);
-      form.style.display = "none";
-    }.bind(this));
+  // Scene_0 : 어서오세요~
+  welcome() {
+    this.draw();
+    let rstrntName = TownCanvas.rstrnts[Restaurant.rstrntIndex].name;
+    this.#ctx.font = "37px dgm";
+    this.#ctx.fillText(`${rstrntName}입니다!`, 330, 687);
   }
 
-  // 메뉴판
+  // Scene_1 : 메뉴판
   scene1(menu) {
     menu.printInfo(this.#ctx);
     let btn = document.getElementById("S1-next");
     btn.style.display = "block";
     btn.addEventListener('click', function () {
       this.#sceneIndex++;
-      let ctx = this.#ctx;
-      let scene = this.#scenes[this.#sceneIndex];
-      let x = this.#x;
-      let y = this.#y;
-      let w = this.#w;
-      let h = this.#h;
-      this.#ctx.drawImage(scene, x, y, w, h);
+      this.draw();
       this.#menu.removeBtn();
       btn.style.display = "none";
     }.bind(this))
   }
 
+  // Scene_9 : 음식 평가
+  scene9(menu) {
+    let name = TownCanvas.rstrnts[Restaurant.rstrntIndex].menus.name[Menu.menuIndex];
+    let price = TownCanvas.rstrnts[Restaurant.rstrntIndex].menus.price[Menu.menuIndex];
+    this.#ctx.font = "35px dgm";
+    // 메뉴명
+    this.#ctx.fillText(name, 122, 530);
+    // 판매가격
+    this.#ctx.fillText(price, 255, 618);
+    // 평가
+    let form = document.getElementById("S9-form");
+    form.style.display = "block";
+    // 입력 박스
+    let input = document.getElementById("S9-input");
+    // 평가 버튼
+    let submit = document.getElementById("S9-submit");
+    submit.addEventListener('click', function () {
+      // if 판매 가격 <= 평가 가격
+      if (price <= input.value) {
+        this.#sceneIndex++; // -> Scene_10
+        this.draw();
+        form.style.display = "none";
+      } else {
+        // else 판매 가격 > 평가 가격
+        this.#sceneIndex += 3; // -> Scene_12
+        this.draw();
+        form.style.display = "none";
+      }
+    }.bind(this));
+
+  }
+  draw() {
+    let scene = this.#scenes[this.#sceneIndex];
+    let x = this.#x;
+    let y = this.#y;
+    let w = this.#w;
+    let h = this.#h;
+    this.#ctx.drawImage(scene, x, y, w, h);
+  }
+
   get canvas() {
     return this.#canvas;
   }
+
 } // class
